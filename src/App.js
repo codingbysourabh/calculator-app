d1955a9cb7dfa3920b39e9475ece1175148168a4
Shubham <sourabhbagade55@gmail.com>
2023-03-08 17:34:16 +0530

    initial comment

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+import Keypad from './Components/Keypad';
+
+function App() {
+  const [ input,setInput] = useState('');
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+  const handleEqual = () => {
+    setInput(eval(input).toString());
+  };
+  const handleClear = () => {
+    setInput("");
+  };
+  return (
+    <div className="App">
+      <div className="calcultor">
+        <input className="display" type="text" value={input} readOnly/>
+        <Keypad
+          handleClear = {handleClear}
+          handleClick = {handleClick}
+          handleEqual = {handleEqual}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
